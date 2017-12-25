7574572f6f110d27d8248e36207d9c6284b3160e
Andy <andreas.wyrobek@acp.de>
2017-12-25 03:19:25 +0100

    add license

--- __manifest__.py
+++ __manifest__.py
@@ -1,3 +1,10 @@
+"""
+Copyright (C) Andreas Wyrobek - All Rights Reserved
+Unauthorized copying of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Andreas Wyrobek <andreas.wyrobek95@gmail.com>, December 2017
+"""
+
 # -*- coding: utf-8 -*-
 {
     'name': "Drag & Drop",
